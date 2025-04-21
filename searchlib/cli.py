4b9bdb7ef180e60f66e44103dc12b6e18062b541
Andrew Plotkin <zarf@ZarfLent.local>
2025-04-21 18:20:25 -0400

    Command-line search cmd.

--- searchlib/cli.py
+++ searchlib/cli.py
@@ -6,7 +6,12 @@ def run(appinstance):
     """
     popt = argparse.ArgumentParser(prog='search.wsgi')
     subopt = popt.add_subparsers(dest='cmd', title='commands')
-    
+
+    popt_search = subopt.add_parser('search', help='perform a search')
+    popt_search.set_defaults(cmdfunc=cmd_search)
+    popt_search.add_argument('query')
+    popt_search.add_argument('-p', '--page', type=int, default=1)
+
     args = popt.parse_args()
 
     if not args.cmd:
@@ -14,3 +19,39 @@ def run(appinstance):
         return
 
     args.cmdfunc(args, appinstance)
+
+def cmd_search(args, app):
+    """Perform a search and display the result(s).
+    """
+    try:
+        query = app.queryparser.parse(args.query)
+    except Exception as ex:
+        print('query parse failed (%s)' % (ex,))
+        return
+    
+    with app.getsearcher() as searcher:
+        results = searcher.search(query)
+        
+        corrected = searcher.correct_query(query, args.query)
+        if corrected.query != query:
+            print('Did you mean: "%s"' % (corrected.string,))
+        
+        if not len(results):
+            print('No results')
+            return
+
+        print('%d results in %.04f sec:' % (len(results), results.runtime,))
+        print()
+        
+        for res in results:
+            fields = res.fields()
+            if 'date' in fields:
+                val = '(%s: %s)' % (fields['type'], fields.get('date'),)
+            else:
+                val = '(%s)' % (fields['type'],)
+            print('* %s  %s' % (fields['path'], val,))
+            if 'shortdesc' in fields:
+                print(fields['shortdesc'])
+            print()
+    
+    
